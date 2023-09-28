20943520b18f6d0fa605c8ef4a65f290fa81a6b4
eddygik7 <edwingicharu8@gmail.com>
2023-09-28 21:53:43 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var a = 4 + 7;
+var b = 5 * 9;
+var c = b / a;
+console.log(c);
+
+let myapples = 0;
+
+if(myapples === 0){
+  myapples += 5;
+  console.log("here you go, now you have", myapples, "apples");
+} else {
+  myapples = 0;
+  console.log("thank you! now you have", myapples, "apples")
+}
+let x = 32;
+if(x === 9){
+  console.log("true");
+}else{
+  console.log("false")
+}
+let y = "green";
+if(y=== "blue"){
+  console.log("blue detected")
+}
+else if
+  (y=== "green"){
+  console.log("green detected")
+}
+  else {
+  console.log("no green or blue detected")
+  };
